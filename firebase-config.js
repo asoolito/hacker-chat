3ab4a732ed3e74dbee5a43989227e6115584e58c
Anonymous_890 <161280470+asoolito@users.noreply.github.com>
2025-06-01 14:50:42 +0500

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,13 +1,12 @@
 // firebase.js
-import { initializeApp } from "https://www.gstatic.com/firebasejs/11.8.1/firebase-app.js";
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.23.0/firebase-app.js";
 import {
   getAuth,
   signInWithEmailAndPassword,
   createUserWithEmailAndPassword,
   onAuthStateChanged,
-  signOut
-} from "https://www.gstatic.com/firebasejs/11.8.1/firebase-auth.js";
-
+  signOut,
+} from "https://www.gstatic.com/firebasejs/9.23.0/firebase-auth.js";
 import {
   getFirestore,
   collection,
@@ -15,15 +14,18 @@ import {
   query,
   orderBy,
   onSnapshot,
-  serverTimestamp
-} from "https://www.gstatic.com/firebasejs/11.8.1/firebase-firestore.js";
+  serverTimestamp,
+  doc,
+  getDoc,
+  setDoc,
+} from "https://www.gstatic.com/firebasejs/9.23.0/firebase-firestore.js";
 
-// ❗ Buni o'zingning Firebase'dagi config bilan almashtir
+// Firebase config (o'zingizning config'ingizni yozing)
 const firebaseConfig = {
   apiKey: "AIzaSyByz_qsV-EcBgnbAbOIRvD9SQD06NcWzyM",
   authDomain: "hacker-chat-4fff2.firebaseapp.com",
   projectId: "hacker-chat-4fff2",
-  storageBucket: "hacker-chat-4fff2.firebasestorage.app",
+  storageBucket: "hacker-chat-4fff2.appspot.com",
   messagingSenderId: "426796888186",
   appId: "1:426796888186:web:f830147b355ceb0cae8bc3"
 };
@@ -44,5 +46,8 @@ export {
   query,
   orderBy,
   onSnapshot,
-  serverTimestamp
+  serverTimestamp,
+  doc,
+  getDoc,
+  setDoc,
 };
